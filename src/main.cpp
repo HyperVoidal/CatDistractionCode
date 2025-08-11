2e155d8f538b86d42ba4500348b248e307f86a00
HyperVoidal <samuel.magri@education.nsw.gov.au>
2025-08-11 17:48:29 +1000

    Updates to main logic for Arduino code
    Added the primary manual movement controls for interpretation from the bluetooth module. Each is localised to a function that is called based on the input from the PC. This is, naturally, only accessible via the manual mode, though I am planning to reuse the functions for the automated movement system since they're so convenient.
    
    Some functions remain as placeholders for the time being, but will be updated accordingly soon enough.
    
    Also, I originally planned to use case switching for the receiver checker, but apparently case switching only works for integers and number values in arduino systems, so I'm stuck with a massive if-else loop unless I can find some way to optimise.

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,127 @@ SoftwareSerial bluetooth(2, 3); // RX, TX
 
 bool Manual = false;
 
+const int IN1=5;
+const int IN2=6;
+const int ENA=9; //needs to be 3, 5, 6, 9, 10, or 11 as these are the PWM specific pins
+const int IN3=7;
+const int IN4=8;
+const int ENA2=11;
+int Speed = 135;
+
 void setup() {
   Serial.begin(9600);    
   bluetooth.begin(9600);
   Serial.println("Arduino Bluetooth Ready!");
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA2, OUTPUT);
 }
 
-void LeftTurn(int direction) {
+void LeftTurn(int direction, int Speed) {
   if (direction == 1) {
-    //Direction is forward
+    //Direction is forward overall
+    //Speed is normal for right side motor
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, Speed);
+    //Speed is halved for left side motor
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA2, constrain((Speed/2), 130, 150)); //speed is minimum 130 maximum 150 to avoid unpowering the motor or going too fast 
+    //both motors rotate in the same direction but right side rotates faster, creating a wide turn arc to the left
+
   } else if (direction == 2) {
     //Direction is backward
+    //Speed is normal but backwards for right side motor
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, Speed);
+    //SPeed is halved for left side motor but backwards
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA2, constrain((Speed/2), 130, 150));
   } else {
-    //Direction is 0 or otherwise, tank remains stationary
+    //Direction is 0 or otherwise, tank remains stationary while turning
+    //M1 (right) rotates forward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, Speed);
+    //M2 (left) rotates backward
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA2, Speed);
   }
 }
 
-void RightTurn(int direction) {
+void RightTurn(int direction, int Speed) {
   if (direction == 1) {
     //Direction is forward
+    //Speed is halved for right side motor
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, constrain((Speed/2), 130, 150));
+    //Speed is normal for left side motor
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA2, Speed);
   } else if (direction == 2) {
     //Direction is backward
+    //Speed is halved for the right side motor
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, constrain((Speed/2), 130, 150));
+    //Speed is normal for left side motor
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA2, Speed);
   } else {
-    //Direction is 0 or otherwise, tank remains stationary
+    //Direction is 0 or otherwise, tank remains stationary while turning
+    //M1 (right) rotates forward
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, Speed);
+    //M2 (left) rotates backward
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA2, Speed);
   }
 }
 
-void Forward(){
-
+void Forward(int Speed){
+  //M1 inputs
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, Speed);
+  //M2 inputs
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA2, Speed);
 }
 
-void Backward(){
-
+void Backward(int Speed){
+  //M1 inputs
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, Speed);
+  //M2 inputs
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA2, Speed);
 }
 
-void ModeSwap(){
-
+void NonManual(){
+  //Main HC-SR04 controls, motor adjustments
+  //Basically all the automated movement systems.
+  //This may need to be split into multiple functions later down the line.
 }
 
 void Laser(){
-
+  //Enable Laser pin or disable laser pin
+  //This function should be a toggle, essentially
 }
 
 void loop() {
@@ -68,24 +149,25 @@ void loop() {
     
     if (Manual == false) {
       //No control functions are accepted
+      NonManual();
     } else {
       //I wanted to do a case switch here but apparently that doesn't work with string comparisons in arduino code
       if (received == "0") {
-        Forward();
+        Forward(Speed);
       } else if (received == "1") {
-        LeftTurn(0);
+        LeftTurn(0, Speed);
       } else if (received == "2") {
-        Backward();
+        Backward(Speed);
       } else if (received == "3") {
-        RightTurn(0);
+        RightTurn(0, Speed);
       } else if (received == "0,1") {
-        LeftTurn(1);
+        LeftTurn(1, Speed);
       } else if (received == "0,3") {
-        RightTurn(1);
+        RightTurn(1, Speed);
       } else if (received == "2,1") {
-        LeftTurn(2);
+        LeftTurn(2, Speed);
       } else if (received == "2,3") {
-        RightTurn(2);
+        RightTurn(2, Speed);
       } else {
         // Do nothing or stop motors
       }
